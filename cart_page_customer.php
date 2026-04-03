c69e2009ac23f3b75edf4ae3008d980853e64ea7
CodeKokeshi <kokeshiaikawa003@gmail.com>
2026-04-03 11:20:03 +0800

    feat: update stylesheet version and add cart order status styles

--- cart_page_customer.php
+++ cart_page_customer.php
@@ -66,7 +66,7 @@ $availableCartItemIds = array_values(array_unique($availableCartItemIds));
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo htmlspecialchars($assetBase, ENT_QUOTES, 'UTF-8'); ?>css/style.css?v=20260319-3">
+    <link rel="stylesheet" href="<?php echo htmlspecialchars($assetBase, ENT_QUOTES, 'UTF-8'); ?>css/style.css?v=20260403-1">
 </head>
 <body class="cart-page">
     <header class="site-header">
